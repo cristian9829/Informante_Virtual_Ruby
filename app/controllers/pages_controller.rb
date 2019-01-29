1a87c837cb77ad6eef7d0420602ea8a44766eda9
cristian <cristian982940@gmail.com>
2019-01-29 17:57:58 -0500

    configurating of device, login signup with styles bootstrap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
+ layout :layout_by_resource
+ 
  def home
  	@noticias = Noticia.where(first_notice: false)
 	@noticia = Noticia.find_by(first_notice: true)
  end
+
+
+  private
+	def layout_by_resource
+		if devise_controller?
+		  "devise"
+		else
+		  "application"
+		end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :noticia, only: [:show ]
   root to: 'pages#home'
